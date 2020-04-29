b13ee3a563291c8678593d3f4cd7e9ab555441ef
gewplay <48548545+gewplay@users.noreply.github.com>
2020-04-29 12:16:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,5 +7,5 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
-echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> ./openwrt/feeds.conf.default
-echo 'src-git tiny https://github.com/TinyTitanPro/lienol-openwrt-package' >> ./openwrt/feeds.conf.default
+echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> ./feeds.conf.default
+echo 'src-git tiny https://github.com/TinyTitanPro/lienol-openwrt-package' >> ./feeds.conf.default
